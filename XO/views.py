75c9092bb0dd50f21c7c7d0ef745d9a073dc317f
Eyad Kabakibo <eyadkabakibo@hotmail.com>
2021-04-21 11:43:41 +0300

    Adding change while click on ticTacToe board

--- XO/views.py
+++ XO/views.py
@@ -3,5 +3,45 @@ from django.http import HttpResponse
 
 # Create your views here.
 #gi
-def index(request):
-    return render(request, "tic.html")
+gaming_list1 = []
+gaming_list2 = []
+gaming_list3 = []
+
+for element in range(3):
+    gaming_list1.append('-')
+    gaming_list2.append('-')
+    gaming_list3.append('-')
+
+states = ' '
+def index(request, num):
+    #lma agy anady 3la el index mn el code adeha ay rakm fo2 el 9
+    global states
+    if num < 3:
+        if  gaming_list1[num] == '-':
+            if states == ' ' or states == 'O':
+                gaming_list1[num]= 'X'
+                states = 'X'
+            else:
+                gaming_list1[num]= 'O'
+                states = 'O'
+    elif num < 6:
+        if gaming_list2[num-3] == '-':
+            if states == ' ' or states == 'O':
+                gaming_list2[num-3]= 'X'
+                states = 'X'
+            else:
+                gaming_list2[num-3]= 'O'
+                states = 'O'
+    elif num < 9:
+        if gaming_list3[num-6] == '-':
+            if states == ' ' or states == 'O':
+                gaming_list3[num-6]= 'X'
+                states = 'X'
+            else:
+                gaming_list3[num-6]= 'O'
+                states = 'O'
+    return render(request, "tic.html",{
+        "game_list1":gaming_list1,
+        "game_list2":gaming_list2,
+        "game_list3":gaming_list3
+    })
